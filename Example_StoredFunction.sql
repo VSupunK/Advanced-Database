53fc023ac0e9ac401866f863a8dc803ed41539cd
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2025-01-04 20:22:20 +0530

    Add stored function to calculate word length and example usage

--- Example_StoredFunction.sql
+++ Example_StoredFunction.sql
@@ -17,10 +17,41 @@ USE stored_function_class;
 
 DELIMITER |
 CREATE FUNCTION full_name(first_name varchar(10), last_name varchar(10))
-RETURN VARCHAR(50)
+RETURNS VARCHAR(50)
 DETERMINISTIC
 BEGIN
     RETURN CONCAT(first_name," ", last_name);
 END |
 
 DELIMITER;
+
+----
+SELECT full_name("Hello", "World") AS full_name;
+
+---------------------------------------------
+
+-- Exercise 02
+
+Utilize the database “stored_function_class ”
+
+Create a MySQL stored function that prompts the user to input a string.
+
+The function should output the total number of characters in the string .
+
+String : “ Sabaragamuwa” – input
+Number of characters : 12 -- Output
+----------------------------
+
+DELIMITER |
+CREATE FUNCTION word_length(Word varchar(50))
+RETURNS INT
+DETERMINISTIC
+BEGIN
+    RETURN length(Word);
+END |
+
+DELIMITER;
+
+----
+SELECT word_length("Hello mysql");
+
