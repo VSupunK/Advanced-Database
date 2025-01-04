b2bca11d753568644a0c0822a1cd5bd8bb04603e
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2025-01-04 20:38:36 +0530

    Add stored function to calculate BMI based on user details

--- Example_StoredFunction.sql
+++ Example_StoredFunction.sql
@@ -53,5 +53,46 @@ END |
 DELIMITER;
 
 ----
-SELECT word_length("Hello mysql");
+SELECT word_length("Hello mysql"); 
+-- OR we can write 
+SELECT word_length("Hello mysql") AS Num_of_characters;
 
+--Exercise 3
+
+CREATE TABLE user_details(User_ID VARCHAR(10), Height DOUBLE, Weight_KG DOUBLE);
+
+INSERT INTO user_details VALUES
+('Usr_01', 1.5, 65.5),
+('Usr_02', 1.7, 72),
+('Usr_03', 1.0, 50.8),
+('Usr_04', 1.85, 61),
+('Usr_05', 1.53, 67);
+
+Write a MySQL stored function that prompts 
+      the user to input the id.
+
+The function should output the BMI value of 
+      the given user.
+
+BMI value will be calculated based on the following equation.
+
+
+--answer--
+
+DELIMITER |
+CREATE FUNCTION Calculate_BMI_Value (ID VARCHAR(10))
+RETURNS DOUBLE
+DETERMINISTIC
+BEGIN
+    DECLARE h,w,BMI_Value DOUBLE;
+    SELECT Height, Weight_KG into h,w FROM user_details WHERE User_ID=ID;
+    SET BMI_Value = (w/(h*h));
+    RETURN BMI_Value;
+
+END |
+
+DELIMITER ;
+
+--
+
+SELECT Calculate_BMI_Value("Usr_04") AS BMI_Value;
