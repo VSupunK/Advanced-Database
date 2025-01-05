24ae61fe00a73eeef38f590ed075f60741e6014e
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2025-01-05 17:50:48 +0530

    Add Student_marks table and avg_marks function for calculating average student scores

---
+++ Tutorial2.sql
@@ -0,0 +1,34 @@
+----Q1
+
+-- i) Write the SQL queries to construct the following table and insert data into the table
+
+CREATE TABLE Student_marks (
+    Student_ID VARCHAR(5) PRIMARY KEY,
+    Math INT,
+    Science INT,
+    English INT
+);
+
+INSERT INTO Student_marks VALUES 
+    ('S102', 78, 81, 81),
+    ('S103', 91, 69, 84),
+    ('S104', 84, 83, 67);
+
+---- ii) Write a MySQL stored function that prompts the user to input the Student_ID. The function should output the average mark of the given student.
+
+DELIMITER | 
+CREATE FUNCTION avg_marks(Stu_ID VARCHAR(5))
+RETURNS DOUBLE
+DETERMINISTIC
+BEGIN
+    DECARE m1 INT, m2 INT, m3 INT, avg DOUBLE;
+    SELECT Match, Science, English INTO FROM m1, m2, m3 FROM Student_marks WHERE Student_ID=Stu_ID;
+    SET avg = (m1+m2+m3)/3;
+    RETURN avg;
+END |
+
+DELIMITER ;
+
+SELECT avg_marks('S102');
+
+
