bbcb2f63e0a991086ff2971add2af6542280880d
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2025-01-05 17:14:28 +0530

    updated

---
+++ Tutorial 1.sql
@@ -0,0 +1,20 @@
+CREATE TABLE techpro (
+    Product VARCHAR(20),
+    Unit_Price DOUBLE
+);
+
+INSERT INTO techpro (Product, Unit_Price) VALUES
+    ('TechBook', 60.00),
+('TechPen', 90.00),
+('TechBag', 450.00),
+('ProPen', 150.00),
+('ProNotebook', 120.00),
+('ProMarker', 80.00);
+
+CREATE TEMPORARY TABLE temp_techpro AS
+SELECT DISTINCT Product AS ProductName, Unit_Price 
+FROM techpro
+WHERE Unit_Price < (SELECT avg(Unit_Price) FROM
+techpro); 
+
+SELECT * FROM temp_techpro;
